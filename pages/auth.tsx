1887582de41d3ade574620ff6b1d87643cc9766d
MirzaUmarAmjad <mirza.umar.amjad@gmail.com>
2023-05-07 17:25:49 -0400

    auth github

--- pages/auth.tsx
+++ pages/auth.tsx
@@ -1,9 +1,14 @@
 import Input from "@/components/input";
 import {useCallback, useState} from "react";
 import axios from "axios";
+import {signIn} from "next-auth/react";
+import {useRouter} from "next/router";
+import {FcGoogle} from "react-icons/fc";
+import { FaGithub} from "react-icons/fa";
 
 export default function Auth() {
 
+    const router = useRouter();
     const loginOptions = {
         login: 'login',
         register: 'register'
@@ -20,14 +25,31 @@ export default function Auth() {
         })
     },[]);
 
+    const login = useCallback(async () => {
+        try {
+            await signIn('credentials',{
+                email,
+                password,
+                redirect: false,
+                callbackUrl: '/'
+            })
+
+            router.push('/');
+        }
+        catch (e) {
+
+        }
+    },[email,password, router]);
+
     const register = useCallback(async () => {
         try {
             await axios.post('/api/register',{email, name, password});
+            await login();
         }
         catch (e) {
             console.log(e);
         }
-    },[email,name,password]);
+    },[email,name,password, login]);
 
     return (
         <div className="relative h-full w-full bg-[url('/images/hero.jpg')] bg-center bg-fixed bg-cover">
@@ -44,7 +66,15 @@ export default function Auth() {
                                 <Input label="Email" onChange={(ev:any) => {setEmail(ev.target.value)}} value={email} id="email" type="email"/>
                                 <Input label="Password" onChange={(ev:any) => {setPassword(ev.target.value)}} value={password} id="password" type="password"/>
                             </div>
-                            <button onClick={register} className="bg-red-600 py-3 text-white rounded-md w-full mt-10 hover:bg-red-700 transition">{variant === loginOptions.login ? 'Login' : 'Sign up' }</button>
+                            <button onClick={ variant === loginOptions.login ? login : register} className="bg-red-600 py-3 text-white rounded-md w-full mt-10 hover:bg-red-700 transition">{variant === loginOptions.login ? 'Login' : 'Sign up' }</button>
+                            <div className="flex flex-row items-center gap-4 mt-8 justify-center">
+                                <div className="w-10 h-10 bg-white rounded-full flex items-center justify-center cursor-pointer hover:opacity-80 transition">
+                                    <FcGoogle size={30} />
+                                </div>
+                                <div onClick={() => signIn('github',{callbackUrl:'/'})} className="w-10 h-10 bg-white rounded-full flex items-center justify-center cursor-pointer hover:opacity-80 transition">
+                                    <FaGithub size={30} />
+                                </div>
+                            </div>
                             <p className="text-neutral-500 mt-12">{variant === loginOptions.login ? 'First time using Netflix?' : 'Already have an account?' }
                                 <span onClick={toggleVariant} className="text-white ml-1 hover:underline cursor-pointer">
                                     {variant === loginOptions.login ? 'create an account?' : 'Login' }
